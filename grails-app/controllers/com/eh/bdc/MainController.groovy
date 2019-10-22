3503a0d5106a38cdf6bad5817b01210f7db93941
emranhos1 <emranhos1@gmail.com>
2019-10-22 21:43:30 +0600

    Create Controller for reset password

--- grails-app/controllers/com/eh/bdc/MainController.groovy
+++ grails-app/controllers/com/eh/bdc/MainController.groovy
@@ -6,5 +6,31 @@ package com.eh.bdc
  */
 class MainController {
 
+    MainService mainService
+    AuthenticationService authenticationService
+
     def index() { }
+
+    def resetPassword(){ }
+
+    def reset(){
+        def response = mainService.matchPassword(params.password)
+        if (!response.isSuccess) {
+            flash.message = AppUtil.infoMessage(g.message(code: "previous.password.not.match"), false)
+            redirect(controller: "main", action: "resetPassword")
+        } else {
+            if(params.password1 == params.password2){
+                Admin admin = authenticationService.getAdmin()
+                admin.setPassword(params.password1)
+                admin.save(flush: true)
+                if (!admin.hasErrors()){
+                    flash.message = AppUtil.infoMessage(g.message(code: "password.changed"), true)
+                    response.isSuccess = true
+                }
+            } else {
+                flash.message = AppUtil.infoMessage(g.message(code: "new.password.not.match"), false)
+            }
+            redirect(controller: "main", action: "resetPassword")
+        }
+    }
 }
